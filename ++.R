8dd269643fc46ed2e2a3d16ed710bf44f21a2431
69ku <144879501+69ku@users.noreply.github.com>
2023-09-27 14:57:01 +0800

    Add files via upload

---
+++ ++.R
@@ -0,0 +1,19 @@
+JohnsFamily <-
+  list(
+    name = "John", 
+    age = 35,
+    spouse = list(
+      name = "Mary", 
+      age = 32),
+    children = list(
+      list(
+        name = "Bill", 
+        age = 5),
+      list(
+        name = "Jane", 
+        age = 3)
+      fncallExample <- list("John", 35, covid19Positive=FALSE)
+      length(fncallExample)
+      names(JohnsFamily[c(1,2)]
+            JohnsFamily[c("name", "age")]fncallExample)
+      
